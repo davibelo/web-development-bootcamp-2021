1e9da7d10e81d65eb24c86afe60126fb763391c9
davibelo <davibelo@gmail.com>
2021-02-04 15:02:08 -0300

    ejs working

--- 08-EJS/00-toDoList/app.js
+++ 08-EJS/00-toDoList/app.js
@@ -1,13 +1,28 @@
 
-const express = require("express")
-const bodyParser = require("body-parser")
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
-const app = express()
+const app = express();
+
+// setting express app to use ejs as view engine
+app.set("view engine", "ejs")
 
 app.get("/", function(req, res){
-    res.send("<h1>Node.js server running...</h1>")
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+    
+    if (currentDay === 6 || currentDay === 0){
+        day = "Weekend";        
+    } else {
+        day = "Weekday";
+    }
+    // rendering html template on list.ejs, substituting
+    // variables as shown below on js object argument
+    res.render("list",{kindOfDay: day}); 
 })
 
 app.listen(3000, function(){
-    console.log("Starting server on port 3000 [OK]")
+    console.log("Starting server on port 3000 [OK]");
 })
