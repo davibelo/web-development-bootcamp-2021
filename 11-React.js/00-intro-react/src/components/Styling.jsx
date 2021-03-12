2d70d65dcd280741dc195ba3df5785e494dc40c5
davibelo <davibelo@gmail.com>
2021-03-12 13:44:14 -0300

    organizing react app example

--- 11-React.js/00-intro-react/src/components/Styling.jsx
+++ 11-React.js/00-intro-react/src/components/Styling.jsx
@@ -7,7 +7,7 @@ const imgSrc = "https://picsum.photos/100";
 
 // You can use inline styling for dynamic styling
 const customStyleGreeting = {
-  color: "red"
+  color: "red",
 };
 
 let greeting = "";
@@ -24,33 +24,33 @@ if (hour < 12) {
 
 // React Component
 function Styling() {
-    return (
+  return (
     <div>
-    <h1 style={customStyleGreeting}>{greeting}</h1>
-    <h1 className="heading">My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
-    <img
-      className="food-img"
-      alt="bacon"
-      src="https://thebakermama.com/wp-content/uploads/2018/08/fullsizeoutput_15a7c.jpg"
-    />
-    <img
-      className="food-img"
-      alt="jamon"
-      src="https://s.cornershopapp.com/product-images/2175214.png?versionId=v3clj.E_M_309IqBq6oajTqsT21OuT8V"
-    />
-    <img
-      className="food-img"
-      alt="noodles"
-      src="https://static.toiimg.com/photo/52467119.cms"
-    />
-    <img className="food-img" alt="random" src={imgSrc + "?grayscale"} />
+      <h1 style={customStyleGreeting}>{greeting}</h1>
+      <h1 className="heading">My Favourite Foods</h1>
+      <ul>
+        <li>Bacon</li>
+        <li>Jamon</li>
+        <li>Noodles</li>
+      </ul>
+      <img
+        className="food-img"
+        alt="bacon"
+        src="https://thebakermama.com/wp-content/uploads/2018/08/fullsizeoutput_15a7c.jpg"
+      />
+      <img
+        className="food-img"
+        alt="jamon"
+        src="https://s.cornershopapp.com/product-images/2175214.png?versionId=v3clj.E_M_309IqBq6oajTqsT21OuT8V"
+      />
+      <img
+        className="food-img"
+        alt="noodles"
+        src="https://static.toiimg.com/photo/52467119.cms"
+      />
+      <img className="food-img" alt="random" src={imgSrc + "?grayscale"} />
     </div>
-    );
-  }
+  );
+}
 
 export default Styling;
