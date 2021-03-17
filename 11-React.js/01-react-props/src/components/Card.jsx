0539d374eb149aae4997fb62ab2c335b236dc087
davibelo <davibelo@gmail.com>
2021-03-17 18:39:25 -0300

    adding Card constructor and mapping

--- 11-React.js/01-react-props/src/components/Card.jsx
+++ 11-React.js/01-react-props/src/components/Card.jsx
@@ -11,6 +11,7 @@ function Card(props) {
       <div className="card">
         <div className="top">
           <h2 className="name">{props.name}</h2>
+          <p>{props.id}</p>
           <Avatar img={props.img} />
         </div>
         <div className="bottom">
