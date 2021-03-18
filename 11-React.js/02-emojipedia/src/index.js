c1f0949342ea3e0d7e7a242785f5d18e131005d1
davibelo <davibelo@gmail.com>
2021-03-17 21:46:58 -0300

    emojipedia start

---
+++ 11-React.js/02-emojipedia/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+// importing React/JSX App
+import App from "./components/App";
+
+// <App/> refers to a React JSX Component called App
+ReactDOM.render(<App />, document.getElementById("root"));
