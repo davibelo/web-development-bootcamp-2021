d8975f615a206b87b3a281239b78a73e3c80e71b
davibelo <davibelo@gmail.com>
2021-03-20 19:02:07 -0300

    using arrow functions

--- 06-javascript/01-basics/04-Functions.js
+++ 06-javascript/01-basics/04-Functions.js
@@ -2,75 +2,85 @@
 // make these instructions easier to reuse
 
 // making function
-function ExitDungeon(){
-    console.log("go left")
-    console.log("go right")
-    console.log("go left")
-    console.log("step out")
+function ExitDungeon() {
+  console.log("go left");
+  console.log("go right");
+  console.log("go left");
+  console.log("step out");
 }
 
 // calling function
-ExitDungeon()
+ExitDungeon();
 
 // making functions with arguments
 function BuyMilk(money) {
-    console.log("go to supermarket")
-    const milk_price = 2.5
-    var num_bottles = money / milk_price
-    num_bottles = Math.floor(num_bottles)
-    // other option to convert to integer:
-    //num_bottles = num_bottles.toFixed(0)
-    console.log("buy " + num_bottles + " bottles of milk")
-    console.log("get home")
+  console.log("go to supermarket");
+  const milk_price = 2.5;
+  var num_bottles = money / milk_price;
+  num_bottles = Math.floor(num_bottles);
+  // other option to convert to integer:
+  //num_bottles = num_bottles.toFixed(0)
+  console.log("buy " + num_bottles + " bottles of milk");
+  console.log("get home");
 }
-BuyMilk(123)
+BuyMilk(123);
 
 // making functions with outputs
 function BuyMilk(money) {
-    console.log("go to supermarket")
-    const milk_price = 2.31
-    var num_bottles = money / milk_price
-    num_bottles = Math.floor(num_bottles)
-    var change = money % milk_price
-    change = change.toFixed(2)
-    console.log("buy " + num_bottles + " bottles of milk")
-    console.log("get " + change + " of change")
-    console.log("get home")
-    return alert("Here are your " + num_bottles + " bottles of milk"
-        + " and your change of $" + change) 
+  console.log("go to supermarket");
+  const milk_price = 2.31;
+  var num_bottles = money / milk_price;
+  num_bottles = Math.floor(num_bottles);
+  var change = money % milk_price;
+  change = change.toFixed(2);
+  console.log("buy " + num_bottles + " bottles of milk");
+  console.log("get " + change + " of change");
+  console.log("get home");
+  return alert(
+    "Here are your " +
+      num_bottles +
+      " bottles of milk" +
+      " and your change of $" +
+      change
+  );
 }
-BuyMilk(300)
+BuyMilk(300);
 
-function BMICalc(weight,height) {
-    var BMI = weight/height**2
-    return Math.round(BMI)
+function BMICalc(weight, height) {
+  var BMI = weight / height ** 2;
+  return Math.round(BMI);
 }
-BMICalc(74,1.78)
-
+BMICalc(74, 1.78);
 
 // dice simulator
 function dice(num_faces) {
-    var rnd_number = 1 + Math.random() * num_faces
-    return Math.floor(rnd_number)
+  var rnd_number = 1 + Math.random() * num_faces;
+  return Math.floor(rnd_number);
 }
-dice(20)
+dice(20);
 
 // Higher order functions
 // Function that uses others functions
 function add(num1, num2) {
-    return num1 + num2;
+  return num1 + num2;
 }
 function subtract(num1, num2) {
-    return num1 - num2;
+  return num1 - num2;
 }
 function multiply(num1, num2) {
-    return num1 * num2;
+  return num1 * num2;
 }
 function divide(num1, num2) {
-    return num1 / num2;
+  return num1 / num2;
 }
 // a function calculator that uses other functions as arguments
 function calculator(num1, num2, operator) {
-    return operator(num1, num2);
+  return operator(num1, num2);
 }
 
+// javascript ES6 arrow functions
+// (input) => (output)
+const BMICalc = (weight, height) => Math.round(weight / height ** 2);
+
+const DaviBMI = BMICalc(75,1.78)
+console.log(DaviBMI);
--- 11-React.js/03-map-filter-reduce-example/src/index.js
+++ 11-React.js/03-map-filter-reduce-example/src/index.js
@@ -1,7 +1,9 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
+import emojipedia from "./emojipedia";
 
-var numbers = [3, 56, 2, 48, 5];
+var numbers = [2, 3, 56, 2, 48, 5];
+console.log(0, numbers);
 
 //Map -Create a new array by doing something with each item in an array.
 
@@ -10,8 +12,8 @@ function double(x) {
 }
 
 // using map method
-let newNumbers = numbers.map(double);
-console.log(newNumbers);
+let newNumbers1 = numbers.map(double);
+console.log(1, newNumbers1);
 
 // using ForEach method
 let newNumbers2 = [];
@@ -19,13 +21,17 @@ numbers.forEach(function (x) {
   let number = double(x);
   newNumbers2.push(number);
 });
-console.log(newNumbers2);
+console.log(2, newNumbers2);
 
-//using map with anonymous callback function
+//using map with callback function
 const newNumbers3 = numbers.map(function (x) {
   return x * 2;
 });
-console.log(newNumbers3);
+console.log(3, newNumbers3);
+
+//using map with arrow callback function
+const newNumbers3a = numbers.map((x) => x * 2);
+console.log("3a", newNumbers3a);
 
 //Filter - Create a new array by keeping the items that return true.
 
@@ -33,37 +39,84 @@ console.log(newNumbers3);
 const newNumbers4 = numbers.filter(function (number) {
   return number > 10;
 });
-console.log(newNumbers4);
+console.log(4, newNumbers4);
+
+// using filter method with arrow function
+const newNumbers4a = numbers.filter((number) => number > 10);
+console.log("4a", newNumbers4a);
 
 //using ForEach to filter
 let newNumbers5 = [];
-numbers.forEach(function (number){
-    if (number > 10){
-        newNumbers5.push(number);
-    }
+numbers.forEach(function (number) {
+  if (number > 10) {
+    newNumbers5.push(number);
+  }
 });
-console.log(newNumbers5);
+console.log(5, newNumbers5);
 
 //Reduce - Accumulate a value by doing something to each item in an array.
 
 // using reduce method
-let newNumbers7 = numbers.reduce(function(accumulator,currentNumber){
-    console.log("accumulator = " + accumulator);
-    console.log("number = " + currentNumber);
-    return (accumulator + currentNumber)
+let newNumbers6 = numbers.reduce(function (accumulator, currentNumber) {
+  console.log("accumulator = " + accumulator);
+  console.log("number = " + currentNumber);
+  return accumulator + currentNumber;
 });
-console.log(newNumbers7);
+console.log(6, newNumbers6);
+
+//using reduce with arrow function
+// using reduce method
+let newNumbers6a = numbers.reduce(
+  (accumulator, currentNumber) => accumulator + currentNumber
+);
+console.log("6a", newNumbers6a);
 
 // using ForEach to reduce
-let newNumbers8 = 0;
-numbers.forEach(function (currentNumber){
-    newNumbers8 += currentNumber;
+let newNumbers7 = 0;
+numbers.forEach(function (currentNumber) {
+  newNumbers7 += currentNumber;
 });
-console.log(newNumbers8);
+console.log(7, newNumbers7);
 
 //Find - find the first item that matches from an array.
 
+const newNumbers8 = numbers.find(function (number) {
+  return number > 10;
+});
+console.log(8, newNumbers8);
+
+// using arrow function
+const newNumbers8a = numbers.find((number) => number > 10);
+console.log("8a", newNumbers8a);
 
 //FindIndex - find the index of the first item that matches.
+
+const newNumbers9 = numbers.findIndex(function (number) {
+  return number > 10;
+});
+console.log(9, newNumbers9);
+
+// using arrow function
+const newNumbers9a = numbers.findIndex((number)=> number > 10);
+console.log("9a", newNumbers9a);
+
+//Exercise
+//Make an array of emojis meaning trunked at 100 characters
+function getMeanings(item) {
+  return item.meaning.substring(0, 100);
+}
+
+const meanings1 = emojipedia.map(getMeanings);
+console.log(10, meanings1);
+
+// using map with callback function
+const meanings2 = emojipedia.map(function (item) {
+  return item.meaning.substring(0, 100);
+});
+console.log(11, meanings2);
+
+// using map with arrow callback function
+const meanings3 = emojipedia.map((item) => item.meaning.substring(0, 100));
+console.log(11, meanings3);
 
 // ReactDOM.render(<App />, document.getElementById("root"));
