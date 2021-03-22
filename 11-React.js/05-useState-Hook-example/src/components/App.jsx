a1f62c1c212e0cc8d733637c59d6d6d0647126d1
davibelo <davibelo@gmail.com>
2021-03-21 22:21:59 -0300

    hooks

---
+++ 11-React.js/05-useState-Hook-example/src/components/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+
+function App() {
+  // useState consists of an array, with a state value and 
+  // a function that updates the state value
+  // useState(x), where x is the initial state value
+  // destructuring useState in a array of count and setCount
+  const [count, setCount] = useState(0);
+
+  function Increase() {
+    setCount(count + 1);
+  }
+
+  function Decrease() {
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={Increase}>+</button>
+      <button onClick={Decrease}>-</button>
+    </div>
+  );
+}
+
+export default App;
