1fd23a5648292ec3d28de8c8605fc31fea9d59c2
davibelo <davibelo@gmail.com>
2021-03-25 13:52:53 -0300

    coment text

--- 11-React.js/04-conditional-render-example/src/components/App.jsx
+++ 11-React.js/04-conditional-render-example/src/components/App.jsx
@@ -8,14 +8,16 @@ let isRegistered = false;
 function App() {
   return (
     <div className="container">
+      <h1>Example 1</h1>
+      <p>Using ternary operator on App.jsx</p>
       {/* ternary operator is a expression not a statement*/}
       {/* so it can be used inside jsx */}
       {/* {condition ? expression if true : expression if false} */}
       {isRegistered ? <Login /> : <Register />}
-      
-      {/* using logic inside component: */}
-      <Form isRegistered={isRegistered} />     
-      
+
+      <h1>Example 2</h1>
+      <p>Using logic inside component</p>
+      <Form isRegistered={isRegistered} />
     </div>
   );
 }
