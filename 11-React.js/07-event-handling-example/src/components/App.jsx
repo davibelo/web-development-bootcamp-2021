f7e48b54a04905c3aae597747bb5536671abd39c
davibelo <davibelo@gmail.com>
2021-03-25 17:41:05 -0300

    event handling in react

--- 11-React.js/07-event-handling-example/src/components/App.jsx
+++ 11-React.js/07-event-handling-example/src/components/App.jsx
@@ -1,9 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
+  let [headingText, setHeadingText] = useState("Hello");
+
+  function handleClick() {
+    setHeadingText("button clicked");
+  }
+
+  // commented bellow is other option of implementation
+
+  let [isMouseOver, setMouseOver] = useState(false);
+  // let [buttonColor, setButtonColor] = useState("white");
+
+  function handleMouseOver() {
+    setMouseOver(true);
+    // setButtonColor("black");
+  }
+
+  function handleMouseOut() {
+    setMouseOver(false);
+    // setButtonColor("white");
+  }
+
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+      <h1>{headingText}</h1>
+      <input type="text" placeholder="What's your name?" />
+      <button
+        onClick={handleClick}
+        onMouseOver={handleMouseOver}
+        onMouseOut={handleMouseOut}
+        style={{ backgroundColor: isMouseOver ? "black" : "white" }}
+        // style={{ backgroundColor: buttonColor }}
+      >
+        Submit
+      </button>
     </div>
   );
 }
