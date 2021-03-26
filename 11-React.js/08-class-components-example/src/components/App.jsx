abb4030970402215722bb066639d0a389d27688c
davibelo <davibelo@gmail.com>
2021-03-26 12:18:25 -0300

    class compomentes example

---
+++ 11-React.js/08-class-components-example/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ClassComponent from "./ClassComponent";
+import FunctionalComponent from "./FunctionalComponent";
+
+function App() {
+  return (
+    <div>
+      <ClassComponent />
+      <FunctionalComponent />
+    </div>
+  );
+}
+
+export default App;
