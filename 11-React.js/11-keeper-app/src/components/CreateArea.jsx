641db941298f58d8679192c2446c5620a0d02ae3
davibelo <davibelo@gmail.com>
2021-03-28 14:08:54 -0300

    expand create area feature

--- 11-React.js/11-keeper-app/src/components/CreateArea.jsx
+++ 11-React.js/11-keeper-app/src/components/CreateArea.jsx
@@ -4,10 +4,15 @@ import Fab from "@material-ui/core/Fab";
 import Zoom from "@material-ui/core/Zoom";
 
 function CreateArea(props) {
+  const [isExpanded, setIsExpanded] = useState(false);
   const [inputNote, setInputNote] = useState({
     title: "",
     content: "",
   });
+
+  function expand() {
+    setIsExpanded(true);
+  }
 
   function handleChange(event) {
     const { value, name } = event.target;
@@ -23,27 +28,31 @@ function CreateArea(props) {
     props.onAdd(inputNote);
     // resetting inputNote
     setInputNote({ title: "", content: "" });
+    // resetting clicked status
+    setIsExpanded(false);
     // preventing <form> to refresh the page
     event.preventDefault();
+    // resetting createArea
   }
-
- 
 
   return (
     <div>
       <form className="create-note">
-        <input
-          name="title"
-          placeholder="Title"
-          value={inputNote.title}
-          onChange={handleChange}
-        />
+        {isExpanded && (
+          <input
+            name="title"
+            placeholder="Title"
+            value={inputNote.title}
+            onChange={handleChange}
+          />
+        )}
         <textarea
           name="content"
           placeholder="Take a note..."
-          rows="3"
+          rows={isExpanded ? 3 : 1}
           value={inputNote.content}
           onChange={handleChange}
+          onClick={expand}
         />
         <Zoom in={true}>
           <Fab aria-label="add" onClick={submitNote}>
