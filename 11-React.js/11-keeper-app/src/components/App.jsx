72228d44d4136665159f1a868a0f5a1951cc7f5c
davibelo <davibelo@gmail.com>
2021-03-27 21:12:49 -0300

    delete note feature

--- 11-React.js/11-keeper-app/src/components/App.jsx
+++ 11-React.js/11-keeper-app/src/components/App.jsx
@@ -5,17 +5,29 @@ import Header from "./Header";
 import CreateArea from "./CreateArea";
 import Note from "./Note";
 import Footer from "./Footer";
-// import notesDB from "../notes";
+import notesDB from "../notes";
 
 function App() {
-  // const [notes, setNotes] = useState(notesDB);
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(notesDB);
+  // const [notes, setNotes] = useState([]);
 
   function addNote(inputNote) {
     setNotes((prevItems) => {
       return [...prevItems, inputNote];
     });
   }
+
+  // the "id" below was passed thru onDelete prop by the NoteComponent
+  function deleteNote(id) {
+    // setting Notes to be the notes excepting the note to be deleted
+    setNotes((prevNotes) => {
+      // filtering all the notes except the note with the id to be deleted
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
@@ -23,7 +35,13 @@ function App() {
       <CreateArea onAdd={addNote} />
       {/* creating notes with map method and a constructor callback */}
       {notes.map((noteItem, index) => (
-        <Note key={index} title={noteItem.title} content={noteItem.content} />
+        <Note
+          key={index}
+          id={index}
+          title={noteItem.title}
+          content={noteItem.content}
+          onDelete={deleteNote}
+        />
       ))}
       <Footer />
     </div>
